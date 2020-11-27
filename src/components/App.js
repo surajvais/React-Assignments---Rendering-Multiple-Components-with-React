a89fe09ea79baedb61884583195db972524bac83
surajvais <37078537+surajvais@users.noreply.github.com>
2020-11-27 14:27:41 +0530

    dsjk

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <div data-ns-test>
+        <h1 data-ns-test="project-name">Medical Related Website</h1>
+        <p data-ns-test="project-description">
+          project descryption is here......
+        </p>
+      </div>
+    </>
+  );
+};
 
 export default App;
